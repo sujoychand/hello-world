da44308d850eaf2e812089e5aad4c965559ba170
sujoychand <testertestingapplications@gmail.com>
2021-12-26 15:41:57 +0530

    Commit tut 1-6 code with harry

---
+++ main.py
@@ -0,0 +1,13 @@
+# First Program
+
+"""This is an alternate way of
+writing
+comments as a multi-line item"""
+print("Jai Shri Ram", end=" ")
+"""
+Default end represents newline character,but = we can use it for anything of choice
+"""
+print("Ayodhya jana hai .. Har har modi")
+
+print("\narry") # output arry , because \n is a newline character
+print("\\narry") # use of escape sequence characters
