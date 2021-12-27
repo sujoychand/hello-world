24302098c296dae0c1ed7f207896467d0e247aaf
sujoychand <testertestingapplications@gmail.com>
2021-12-27 18:03:45 +0530

    added solve_3.py

---
+++ solve_3.py
@@ -0,0 +1,13 @@
+# Code with Harry : Problem Solve 3
+
+print("Enter Age of the person")
+age = int(input())
+
+if 18 < age <= 100:
+    print("Person can drive the car")
+if age == 18:
+    print("Not able to decide weather person can drive or not")
+elif 7 <= age < 18:
+    print("Too Young to think about driving")
+else:
+    print("Person cannot drive the car")
