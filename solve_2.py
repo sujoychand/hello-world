4bd1514bc4013da8c1be26315d83c47460e525d7
sujoychand <testertestingapplications@gmail.com>
2021-12-27 14:14:54 +0530

    Git Commit for dictonary problem by code with Harry solution

---
+++ solve_2.py
@@ -0,0 +1,13 @@
+#Code with Harry : Problem Solve 2
+
+dictionary_1 = {
+    "Person_1" : "Ram",
+    "Person_2" : "Sham",
+    "Person_3" : "RK Sinha",
+    "Person_4" : "HK Sinha"
+}
+print(dictionary_1)
+print("Enter word for dictionary")
+a = input(str())
+print(dictionary_1.get(a))
+
