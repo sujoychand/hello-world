4aa3baf5e12df9b79beee4062b8190644287cb29
sujoychand <testertestingapplications@gmail.com>
2021-12-29 11:25:39 +0530

    directly staged

---
+++ solve_5.py
@@ -0,0 +1,11 @@
+# Code withHarry : Solve 5 : Solved through video watching
+list1 = ["Harry", 2, "100", 200]
+for item in list1:
+    if str(item).isnumeric():  # Trick was to use str function with isnumeric function
+        print("Item is an Integer ", item)
+    else:
+        print("Item is NOT an Integer ", item)
+
+
+
+
